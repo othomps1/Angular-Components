c1ec3cad7bec7733a095ea7f96ec4e8f0a22d401
Owen Thompson <othomps1986@gmail.com>
2020-02-05 21:07:04 -0700

    Added hooks and final components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
 
   onServerAdded(serverData: {serverName: string, serverContent: string}) {
-      this.serverElements.push({
+    this.serverElements.push({
       type: 'server',
       name: serverData.serverName,
       content: serverData.serverContent
@@ -17,10 +17,18 @@ export class AppComponent {
   }
 
   onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
-      this.serverElements.push({
+    this.serverElements.push({
       type: 'blueprint',
       name: blueprintData.serverName,
       content: blueprintData.serverContent
     });
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
   }
 }
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -10,24 +10,25 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   // newServerName = '';
   // newServerContent = '';
-  @ViewChild('serverContentInput') serverContentInput: ElementRef;
+  @ViewChild('serverContentInput', { static: false }) serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
-  
+
   onAddServer(nameInput: HTMLInputElement) {
-   this.serverCreated.emit({ 
-   serverName: nameInput.value, 
-   serverContent: this.serverContent.nativeElement.value;
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
-   this.blueprintCreated.emit({
-    serverName: nameInput.value, 
-    serverContent: this.serverContent.nativeElement.value});
-   }
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
+    });
+  }
 
 }
